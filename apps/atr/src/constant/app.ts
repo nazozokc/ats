e026180b3716a50fbb3c07a5a55fb9722f6677fa
nazozokc <nazozokc@users.noreply.github.com>
2026-09-26 11:00:12 +0900

    edit

--- apps/atr/src/constant/app.ts
+++ apps/atr/src/constant/app.ts
@@ -1,6 +1,6 @@
 import { homedir } from "os";
 import { join } from "path";
 
-export const CLI_COMMAND_NAME = "ats";
-export const ATS_ROOT_DIR = join(`${homedir()}`, ".config", "ats");
-export const ATS_SETTING_DIR = join(`${homedir()}`, ".config", "ats", "tomls");
+export const CLI_COMMAND_NAME = "atr";
+export const ATR_ROOT_DIR = join(`${homedir()}`, ".config", "atr");
+export const ATR_SETTING_DIR = join(`${homedir()}`, ".config", "atr", "tomls");
